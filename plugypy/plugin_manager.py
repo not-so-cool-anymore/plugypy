146e341bd86f9b601549b4f747356db48cafa0a3
Ivan Zlatanov <ivanff1@abv.bg>
2020-06-20 00:04:29 +0300

    Importing bug was fixed.

--- plugypy/plugin_manager.py
+++ plugypy/plugin_manager.py
@@ -2,7 +2,7 @@ import os
 import pwd
 import sys
 import json
-from plugypy import MainFunctionNotFoundError
+from .plugypy_errors import MainFunctionNotFoundError
 
 class PluginManager():
     def __init__(self, plugins_folder_location, config_file_location):
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.MD') as readme:
 
 setuptools.setup(
     name='PlugyPy',
-    version='0.1.4',
+    version='0.1',
     author='Ivan Zlatanov',
     author_email='me@iv.an',
     description='A lightweigh Python plugin system',

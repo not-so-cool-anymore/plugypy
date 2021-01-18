78d25f3593a4433dee2cf9ad57d42c9878ca843d
Ivan Zlatanov <ivanff1@abv.bg>
2021-01-18 15:26:40 +0200

    __str__ function was added to Plugin and Configuration.

--- plugypy/configuration.py
+++ plugypy/configuration.py
@@ -1,10 +1,23 @@
 from typing import List
 from .plugin import Plugin
 
+
 class Configuration(object):
     will_load_all: bool
     plugins: List[Plugin]
 
     def __init__(self, will_load_all: bool, plugins: List[Plugin]):
-        self.plugins = plugins
         self.will_load_all = will_load_all
+        self.plugins = plugins
+
+    def __str__(self):
+        configuration_string = 'PlugyPy Plugins Configuration:\n'
+        configuration_string += '\tWill Load All Plugins: {}\n'.format(
+            self.will_load_all)
+
+        for plugin in self.plugins:
+            configuration_string += '\t\tPlugin Name: {}\n'.format(plugin.name)
+            configuration_string += '\t\tIs Enabled: {}\n'.format(
+                plugin.is_enabled)
+
+        return configuration_string
--- plugypy/plugin.py
+++ plugypy/plugin.py
@@ -5,3 +5,9 @@ class Plugin(object):
     def __init__(self, name: str, is_enabled: bool):
         self.name = name
         self.is_enabled = is_enabled
+
+    def __str__(self):
+        return 'Plugin Name: {}\nIs Enabled: {}\n'.format(
+            self.name,
+            self.is_enabled
+        )

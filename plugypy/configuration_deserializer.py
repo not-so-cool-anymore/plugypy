54a205a61b9fe22bff2574d70c607b4b70726ac9
Ivan Zlatanov <ivanff1@abv.bg>
2021-02-03 13:07:55 +0200

    Configuration deserializer was updated.

--- plugypy/configuration_deserializer.py
+++ plugypy/configuration_deserializer.py
@@ -1,19 +1,35 @@
 import json
-from .plugypy_errors import  InvalidConfigurationFile
+from .plugypy_errors import InvalidConfigurationFile
 from .configuration import Configuration
+from .plugin import Plugin
+
 
 class ConfigurationDeserializer():
     def __init__(self, config_file_location):
-        self.__config_file_location =  config_file_location
-    
+        self.__config_file_location = config_file_location
+
     def deserialize_config(self):
         config = None
 
         with open(self.__config_file_location, 'r') as config_file:
+            plugins_list = list()
+
             try:
                 config_json = json.loads(config_file.read())
-                config = Configuration(**config_json)
-            except: 
+
+                for _plugin in config_json['plugins']:
+                    plugin = Plugin(
+                        _plugin['name'],
+                        _plugin['is_enabled']
+                    )
+
+                    plugins_list.append(plugin)
+
+                config = Configuration(
+                    config_json['will_load_all'],
+                    plugins_list
+                )
+            except:
                 raise InvalidConfigurationFile() from None
 
         return config
